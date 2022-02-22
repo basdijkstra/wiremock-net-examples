9e875b14650fb99cd162249d7543d42696c7d2e1
Bas Dijkstra <bas@ontestautomation.com>
2022-02-22 16:46:36 +0100

    Initial commit with first example

---
+++ WireMockNetExamples/BasicExample.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using RestSharp;
+using System.Net;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace WireMockNetExamples
+{
+    [TestFixture]
+    public class BasicExample
+    {
+        private WireMockServer server;
+
+        private RestClient client;
+
+        private const string BASE_URL = "http://localhost:9876";
+
+        [OneTimeSetUp]
+        public void SetupRestSharpClient()
+        {
+            client = new RestClient(BASE_URL);
+        }
+
+        [SetUp]
+        public void StartServer()
+        {
+            server = WireMockServer.Start(9876);
+        }
+
+        public void CreateHelloWorldStub()
+        {
+            server.Given(
+                Request.Create().WithPath("/hello-world").UsingGet()
+            )
+            .RespondWith(
+                Response.Create()
+                .WithStatusCode(200)
+                .WithHeader("Content-Type", "text/plain")
+                .WithBody("Hello, world!")
+            );
+        }
+
+        [Test]
+        public async Task TestHelloWorldStub()
+        {
+            CreateHelloWorldStub();
+
+            RestRequest request = new RestRequest("/hello-world", Method.Get);
+
+            RestResponse response = await client.ExecuteAsync(request);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.That(response.ContentType, Is.EqualTo("text/plain"));
+            Assert.That(response.Content, Is.EqualTo("Hello, world!"));
+        }
+
+        [TearDown]
+        public void StopServer()
+        {
+            server.Stop();
+        }
+
+    }
+}
